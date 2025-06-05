f6bbfb3b7f0deb6b602420d33a3618b66721dd45
Lazar Nikolov <lazar.nikolov@sentry.io>
2025-06-05 15:02:29 -0400

    upgraded to tailwind v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     sentryVitePlugin({
       org: process.env.VITE_SENTRY_ORG,
       project: process.env.VITE_SENTRY_PROJECT,
