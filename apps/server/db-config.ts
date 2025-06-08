943370d71d8b4e5ff5567e5a852c4f92ab50c75f
Lazar Nikolov <lazar.nikolov@sentry.io>
2025-06-08 16:42:10 -0400

    removing redundant drizzle client creation

--- apps/server/db-config.ts
+++ apps/server/db-config.ts
@@ -1,4 +1,3 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as dotenv from 'dotenv';
 
@@ -7,8 +6,6 @@ dotenv.config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
-
-export const db = drizzle(pool);
 
 // Event listeners for pool
 pool.on('error', (err: any) => {
